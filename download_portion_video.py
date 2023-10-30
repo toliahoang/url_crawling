7f4ad12e3c2108ce90c92d37dfa6d1f22893e56a
toliahoang <tung.istu@gmail.com>
2023-10-30 16:37:40 +0700

    Update download_portion_video.py

--- download_portion_video.py
+++ download_portion_video.py
@@ -3,6 +3,21 @@ import math
 from functools import wraps
 import yt_dlp
 from yt_dlp.utils import download_range_func
+import logging
+
+
+
+logger = logging.basicConfig(filename='log_file_name.log',level=logging.INFO)
+
+
+def time_processing(f):
+    def wrapped(*args, **kwargs):
+        begin = time.time()
+        f(*args, **kwargs)
+        end = time.time()
+        print(f"processing time of {f.__name__} is: {end - begin}")
+        logging.info(f"processing time {end - begin}")
+    return wrapped
 
 
 def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
@@ -36,7 +51,8 @@ def convert_hhmmss_to_second(time_str):
     return int(h) * 3600 + int(m) * 60 + int(s)
 
 
-@retry(Exception, tries=4)
+@time_processing
+@retry(Exception, tries=6)
 def download_file(url, start_time_str, end_time_str):
     start_time = int(convert_hhmmss_to_second(start_time_str))
     end_time = int(convert_hhmmss_to_second(end_time_str))
@@ -45,12 +61,27 @@ def download_file(url, start_time_str, end_time_str):
         "ytsearch": True,
         'verbose': True,
         'download_ranges': download_range_func(None, [(start_time, end_time)]),
-        'force_keyframes_at_cuts': True,
+        'force_keyframes_at_cuts': False,
         'format':"mp4"
     }
     with yt_dlp.YoutubeDL(yt_opts) as ydl:
         ydl.download(f"{url}")
 
 
-download_file('https://www.twitch.tv/videos/1961098587', "00:30:00", "00:30:10")
 
+
+list_videos = ['https://www.twitch.tv/videos/1959544588',
+               'https://www.twitch.tv/videos/1963260122',
+               'https://www.twitch.tv/videos/1958434416',
+               'https://www.twitch.tv/videos/1962786599',
+               'https://www.twitch.tv/videos/1960683307',
+               'https://www.twitch.tv/videos/1960945044']
+
+
+@time_processing
+def download_list_url(list_videos):
+    for url in list_videos:
+        download_file(url, "00:00:10", "00:00:60")
+
+
+download_list_url(list_videos)
